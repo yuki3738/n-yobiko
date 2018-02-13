46a815e3926ab5dffad87f742146e1233069c32a
yuki3738 <y.minamiya@gmail.com>
2018-02-13 10:50:10 +0900

    18.Cookie を使った秘密の匿名掲示板

---
+++ workspace/cookie-study/index.js
@@ -0,0 +1,12 @@
+'use strict'
+const http = require('http');
+const server = http.createServer((req, res) => {
+    const now = new Date().getTime();
+    res.setHeader('Set-Cookie', 'last_access=' + now + ';');
+    const last_access_time = req.headers.cookie ? parseInt(req.headers.cookie.split('last_access=')[1]) : now;
+    res.end(new Date(last_access_time).toString());
+});
+const port = 8000;
+server.listen(port, () => {
+    console.info('Listening on ' + port);
+});
