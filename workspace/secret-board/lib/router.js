ecf575e345f6ada12c2176151f6db8f7d85f968a
yuki3738 <y.minamiya@gmail.com>
2018-02-13 22:31:25 +0900

    20.フォームによる投稿機能の実装

---
+++ workspace/secret-board/lib/router.js
@@ -0,0 +1,16 @@
+'use strict';
+const postsHandler = require('./posts-handler');
+
+function route(req, res) {
+  switch (req.url) {
+    case '/posts':
+      postsHandler.handle(req, res);
+      break;
+    default:
+      break;
+  }
+}
+
+module.exports = {
+  route: route
+};
